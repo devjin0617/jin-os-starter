50be2da977d2f01363a5018e7cda7d92d5d0e5de
devjin0617 <devjin0617@gmail.com>
2017-01-03 00:16:47 +0900

    init

---
+++ src/TaskBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        {{ msg }}
+        <div class="task-bar"></div>
+    </div>
+</template>
+<style>
+    .task-bar {
+        width:100%;
+        height:50px;
+        background-color:#010409;
+        position:fixed;
+        bottom:0;
+    }
+</style>
+<script>
+
+    export default{
+        data() {
+            return hello
+        }
+    }
+</script>
